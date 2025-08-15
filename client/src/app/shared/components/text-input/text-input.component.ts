df7ac719d121dbcfaa06feffcbe8677bf10d9f4e
shafiq0225 <shafiqahamed.be@gmail.com>
2025-08-15 09:32:15 +0530

    End of section 15

---
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+@Component({
+  selector: 'app-text-input',
+  imports: [
+    MatFormField,
+    MatInput,
+    MatError,
+    MatLabel,
+    ReactiveFormsModule
+  ],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss'
+})
+export class TextInputComponent implements ControlValueAccessor {
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+  }
+  writeValue(obj: any): void { }
+  registerOnChange(fn: any): void { }
+  registerOnTouched(fn: any): void { }
+  setDisabledState?(isDisabled: boolean): void { }
+  @Input() label = '';
+  @Input() type = 'text';
+
+  get control() {
+      return this.controlDir.control as FormControl;
+    }
+}
